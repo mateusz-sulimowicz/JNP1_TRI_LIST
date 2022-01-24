02338fe4b0e335b39a11a846bdc2e1515240cfe8
mateusz <ms429603@students.mimuw.edu.pl>
2022-01-24 14:08:05 +0100

    final

--- tri_list.h
+++ tri_list.h
@@ -5,6 +5,7 @@
 #include <variant>
 #include <vector>
 #include <ranges>
+#include <tuple>
 #include <functional>
 #include "tri_list_concepts.h"
 
@@ -20,19 +21,17 @@ constinit static const auto identity = [](T x) { return x; };
 
 template<typename T, modifier<T> F, modifier<T> G>
 std::function<T(T)> compose(F f, G g) {
-    return [=](T x) {
-        return f(g(x));
-    };
+    return [=](T x) { return f(g(x)); };
 };
 
 template<typename T1, typename T2, typename T3>
 class tri_list {
     using var_t = std::variant<T1, T2, T3>;
-    using t1_modifier_t = std::function<T1(T1)>;
-    using t2_modifier_t = std::function<T2(T2)>;
-    using t3_modifier_t = std::function<T3(T3)>;
 
-    class iterator : public std::vector<var_t>::iterator {
+    template<typename T>
+    using mod = std::function<T(T)>;
+
+    class iterator {
         using iter_t = typename std::vector<var_t>::iterator;
     public:
         using iterator_category = std::forward_iterator_tag;
@@ -43,11 +42,9 @@ class tri_list {
 
         iterator() = default;
 
-        explicit iterator(const t1_modifier_t &m1,
-                          const t2_modifier_t &m2,
-                          const t3_modifier_t &m3,
+        explicit iterator(const std::tuple<mod<T1>, mod<T2>, mod<T3>> &m,
                           iter_t original) noexcept
-                : m1(m1), m2(m2), m3(m3), it(original) {}
+                : modifiers(m), it(original) {}
 
         iterator &operator++() noexcept {
             ++it;
@@ -88,18 +85,16 @@ class tri_list {
         }
 
     private:
-        t1_modifier_t m1 = identity<T1>;
-        t2_modifier_t m2 = identity<T2>;
-        t3_modifier_t m3 = identity<T3>;
+        std::tuple<mod<T1>, mod<T2>, mod<T3>> modifiers = {identity<T1>, identity<T2>, identity<T3>};
         iter_t it = std::vector<var_t>().begin();
 
         var_t modify(var_t v) const {
             if (std::holds_alternative<T1>(v)) {
-                return var_t{m1(std::get<T1>(v))};
+                return var_t{std::get<mod<T1>>(modifiers)(std::get<T1>(v))};
             } else if (std::holds_alternative<T2>(v)) {
-                return var_t{m2(std::get<T2>(v))};
+                return var_t{std::get<mod<T2>>(modifiers)(std::get<T2>(v))};
             } else {
-                return var_t{m3(std::get<T3>(v))};
+                return var_t{std::get<mod<T3>>(modifiers)(std::get<T3>(v))};
             }
         }
     };
@@ -116,60 +111,32 @@ public:
 
     template<one_of<T1, T2, T3> T, modifier<T> F>
     void modify_only(F m = F{}) {
-        if constexpr (std::is_same_v<T, T1>) {
-            m1 = compose<T>(m, m1);
-        } else if constexpr (std::is_same_v<T, T2>) {
-            m2 = compose<T>(m, m2);
-        } else if constexpr (std::is_same_v<T, T3>) {
-            m3 = compose<T>(m, m3);
-        }
+        std::get<mod<T>>(modifiers) = compose<T>(m, std::get<mod<T>>(modifiers));
     }
 
     template<one_of<T1, T2, T3> T>
     void reset() {
-        if constexpr(std::is_same_v<T, T1>) {
-            m1 = identity<T1>;
-        } else if constexpr(std::is_same_v<T, T2>) {
-            m2 = identity<T2>;
-        } else if constexpr(std::is_same_v<T, T3>) {
-            m3 = identity<T3>;
-        }
+        std::get<mod<T>>(modifiers) = identity<T>;
     }
 
     template<one_of<T1, T2, T3> T>
     auto range_over() {
         return content
                | std::views::filter([](var_t v) { return std::holds_alternative<T>(v); })
-               | std::views::transform([this](var_t v) { return get_modifier<T>()(std::get<T>(v)); });
+               | std::views::transform([this](var_t v) { return std::get<mod<T>>(modifiers)(std::get<T>(v)); });
     }
 
     auto begin() {
-        return iterator(m1, m2, m3, content.begin());
+        return iterator(modifiers, content.begin());
     }
 
     auto end() {
-        return iterator(m1, m2, m3, content.end());
+        return iterator(modifiers, content.end());
     }
 
 private:
     std::vector<var_t> content;
-    t1_modifier_t m1 = identity<T1>;
-    t2_modifier_t m2 = identity<T2>;
-    t3_modifier_t m3 = identity<T3>;
-
-    template<typename T>
-    requires one_of<T, T1, T2, T3>
-    auto get_modifier() const {
-        if constexpr (std::is_same_v<T, T1>) {
-            return m1;
-        } else if constexpr (std::is_same_v<T, T2>) {
-            return m2;
-        } else if constexpr (std::is_same_v<T, T2>) {
-            return m3;
-        } else {
-            return identity<T>;
-        }
-    }
+    std::tuple<mod<T1>, mod<T2>, mod<T3>> modifiers = {identity<T1>, identity<T2>, identity<T3>};
 };
 
 #endif //TRI_LIST_H
